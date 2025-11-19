2a77964257489d80f8a95666f3fcc9434f0260e2
Seohyun Kim <canna7610@gmail.com>
2025-11-19 23:29:40 +0900

    fix: 카테고리에 방탈출/뮤지컬/콘서트 추가 (하드 코딩)

--- deokmoa/app/src/main/java/com/example/deokmoa/data/Category.kt
+++ deokmoa/app/src/main/java/com/example/deokmoa/data/Category.kt
@@ -6,5 +6,7 @@ enum class Category(val displayName: String) {
     NOVEL("소설"),
     DRAMA("드라마"),
     MOVIE("영화"),
-    OTHER("기타")
+    ROOM_ESCAPE("방탈출"),
+    MUSICAL("뮤지컬"),
+    CONCERT("콘서트")
 }
